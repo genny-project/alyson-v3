14a05aa5cb890a1e916a6847292f682880d2120c
zcallan <kaln.delbridge@hotmail.com>
2018-05-02 18:43:33 +1000

    added redux-observable middleware

---
+++ src/redux/epics.js
@@ -0,0 +1,8 @@
+import { combineEpics } from 'redux-observable';
+// export { default as exampleEpic } from '../my/path';
+
+const epics = combineEpics(
+  // exampleEpic
+);
+
+export default epics;
--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,12 +1,17 @@
 import { applyMiddleware } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
 import logger from 'redux-logger';
+import epics from './epics';
+
+const epicMiddleware = createEpicMiddleware( epics );
 
 const developmentMiddleware = [
+  epicMiddleware,
   logger,
 ];
 
 const productionMiddleware = [
-
+  epicMiddleware,
 ];
 
 export default applyMiddleware(
