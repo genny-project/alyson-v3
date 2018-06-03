0059d4701ffd3d8ae251eb09c4dd6902e9b79b98
zcallan <kaln.delbridge@hotmail.com>
2018-06-03 21:41:18 +1000

    improved router to filter and sort layouts based on wildcards

--- src/views/pages/generic/LayoutFetcher.js
+++ src/views/pages/generic/LayoutFetcher.js
@@ -26,7 +26,11 @@ class LayoutFetcher extends Component {
 
   getLayout() {
     const { attributes } = this.props.baseEntities;
-    const layoutAttribute = Object.keys( attributes ).find( this.findAttribute );
+    const layoutAttribute = Object
+      .keys( attributes )
+      .filter( this.handleFilterAttributes )
+      .sort( this.handleSortAttributes )
+      .find( this.findAttribute );
 
     const layout = (
       layoutAttribute &&
@@ -49,6 +53,70 @@ class LayoutFetcher extends Component {
     }
   }
 
+  handleFilterAttributes = attribute => {
+    if ( attribute.startsWith( 'LAY_' ))
+      return true;
+
+    return false;
+  }
+
+  /**
+   * Sort the attributes so that the routes which contain
+   * a colon (`:`) or a splat / asterisk (`*`) always come
+   * last in the list.
+   *
+   * For example, if we had `load/add` and `load/:id` as our
+   * 2 routes, we want to see if the current URL would match
+   * `load/add` before the second route as the second route
+   * is a wildcard, and anything will match it.
+   *
+   * TODO: expand this check further so it better compares
+   * routes containing multiple colons and asterisks
+   *
+   * NB: return numbers:
+   *   0 - keep the same order
+   *   1 - Put A after B
+   *   -1 - Put B after A
+   */
+  handleSortAttributes = ( attributeA, attributeB ) => {
+    const { attributes } = this.props.baseEntities;
+
+    const routeA = (
+      attributes[attributeA] &&
+      attributes[attributeA].PRI_LAYOUT_URI &&
+      attributes[attributeA].PRI_LAYOUT_URI.valueString
+    );
+
+    if ( !routeA )
+      return 0;
+
+    const routeB = (
+      attributes[attributeB] &&
+      attributes[attributeB].PRI_LAYOUT_URI &&
+      attributes[attributeB].PRI_LAYOUT_URI.valueString
+    );
+
+    if ( !routeB )
+      return 0;
+
+    if (
+      routeA.includes( ':' ) &&
+      routeB.includes( ':' )
+    ) {
+      return 0;
+    }
+
+    /* Put routeA after layoutB. */
+    if ( routeA.includes( ':' ))
+      return 1;
+
+    /* Put routeB after layoutA. */
+    if ( routeB.includes( ':' ))
+      return -1;
+
+    return 0;
+  }
+
   handleMapUrlFragments = currentUrlFragments => ( fragment, index ) => {
     if ( fragment.startsWith( ':' )) {
       return currentUrlFragments[index];
@@ -62,38 +130,36 @@ class LayoutFetcher extends Component {
     const { attributes } = baseEntities;
     const strippedCurrentUrl = removeStartingAndEndingSlashes( currentUrl );
 
-    if ( attribute.startsWith( 'LAY_' )) {
-      const layoutUrl = (
-        attributes[attribute] &&
-        attributes[attribute].PRI_LAYOUT_URI &&
-        attributes[attribute].PRI_LAYOUT_URI.valueString
-      );
+    const layoutUrl = (
+      attributes[attribute] &&
+      attributes[attribute].PRI_LAYOUT_URI &&
+      attributes[attribute].PRI_LAYOUT_URI.valueString
+    );
 
-      if ( !layoutUrl ) {
-        return false;
-      }
-
-      let strippedLayoutUrl = removeStartingAndEndingSlashes( layoutUrl );
-
-      if ( strippedLayoutUrl.includes( ':' )) {
-        const layoutUrlFragments = strippedLayoutUrl.split( '/' );
-        const currentUrlFragments = strippedCurrentUrl.split( '/' );
-
-        if ( layoutUrlFragments.length !== currentUrlFragments.length )
-          return;
-
-        strippedLayoutUrl =
-          layoutUrlFragments
-            .map( this.handleMapUrlFragments( currentUrlFragments ))
-            .join( '/' );
-      }
-
-      if ( strippedLayoutUrl === strippedCurrentUrl ) {
-        return true;
-      }
+    if ( !layoutUrl ) {
+      return false;
     }
 
-    return false;
+    let strippedLayoutUrl = removeStartingAndEndingSlashes( layoutUrl );
+
+    console.warn({ strippedLayoutUrl, strippedCurrentUrl });
+
+    if ( strippedLayoutUrl.includes( ':' )) {
+      const layoutUrlFragments = strippedLayoutUrl.split( '/' );
+      const currentUrlFragments = strippedCurrentUrl.split( '/' );
+
+      if ( layoutUrlFragments.length !== currentUrlFragments.length )
+        return false;
+
+      strippedLayoutUrl =
+        layoutUrlFragments
+          .map( this.handleMapUrlFragments( currentUrlFragments ))
+          .join( '/' );
+    }
+
+    if ( strippedLayoutUrl === strippedCurrentUrl ) {
+      return true;
+    }
   };
 
   render() {
