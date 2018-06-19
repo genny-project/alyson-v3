512b489b326546e9d15e5545b06f7958107e0da9
zcallan <kaln.delbridge@hotmail.com>
2018-06-19 17:05:14 +1000

    added CreditCard component

---
+++ src/views/components/credit-card/CreditCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { string, number, object } from 'prop-types';
+import { CardView } from 'react-native-credit-card-input';
+
+const CreditCard = ({
+  focused,
+  brand,
+  name,
+  number,
+  expiry,
+  cvc,
+  placeholder,
+  scale,
+  fontFamily,
+  imageFront,
+  imageBack,
+  customIcons,
+}) => (
+  <CardView
+    focused={focused}
+    brand={brand}
+    name={name}
+    number={number}
+    expiry={expiry}
+    cvc={cvc}
+    placeholder={placeholder}
+    scale={scale}
+    fontFamily={fontFamily}
+    imageFront={imageFront}
+    imageBack={imageBack}
+    customIcons={customIcons}
+  />
+);
+
+CreditCard.propTypes = {
+  focused: string,
+  brand: string,
+  name: string,
+  number: string,
+  expiry: string,
+  cvc: string,
+  placeholder: object,
+  scale: number,
+  fontFamily: string,
+  imageFront: number,
+  imageBack: number,
+  customIcons: object,
+};
+
+export default CreditCard;
--- src/views/components/index.js
+++ src/views/components/index.js
@@ -39,6 +39,7 @@ export { default as EventButton } from './event-button';
 export { default as EventButtonLink } from './event-button-link';
 export { default as Payments } from './payments';
 export { default as WebView } from './web-view';
+export { default as CreditCard } from './credit-card';
 
 export { default as alert } from './alert';
 
