a0a860f5ec032b1aa7b9ef4062a3734a436055e0
zcallan <kaln.delbridge@hotmail.com>
2018-06-25 17:36:32 +1000

    added directions to Maps native, now works with props, misc map fixes

--- src/views/components/map/Map.native.js
+++ src/views/components/map/Map.native.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Dimensions } from 'react-native';
 import { MapView } from 'expo';
-import { array } from 'prop-types';
+import { array, bool } from 'prop-types';
+import MapViewDirections from 'react-native-maps-directions';
+import config from '../../../config';
 import { Box } from '../';
 
 class Map extends Component {
   static propTypes = {
     markers: array,
+    showDirections: bool,
+  }
+
+  mapStyle = {
+    height: '100%',
+    width: '100%',
+    flex: 1,
+  }
+
+  handleReady = result => {
+    const { width, height } = Dimensions.get( 'window' );
+
+    this.mapView.fitToCoordinates( result.coordinates, {
+      edgePadding: {
+        right: ( width / 30 ),
+        bottom: ( height / 30 ),
+        left: ( width / 30 ),
+        top: ( height / 30 ),
+      },
+    });
   }
 
   render() {
-    const { markers } = this.props;
-
-    const markerComponents = markers && markers.map(( marker, index ) => (
-      <MapView.Marker
-        key={index} // eslint-disable-line
-        coordinate={{
-          latitude: marker.latitude,
-          longitude: marker.longitude,
-        }}
-      />
-    ));
+    const { markers, showDirections } = this.props;
 
     return (
       <Box
@@ -29,29 +42,43 @@ class Map extends Component {
         flex={1}
       >
         <MapView
-          style={{
-            height: '100%',
-            width: '100%',
-            flex: 1,
-          }}
+          ref={mapView => this.mapView = mapView}
+          style={this.mapStyle}
         >
-          {markerComponents}
+          {(
+            markers &&
+            markers instanceof Array &&
+            markers.length > 0
+          ) ? (
+            <Fragment>
+              {markers.map( marker => (
+                <MapView.Marker
+                  key={marker.key}
+                  coordinate={{
+                    latitude: marker.latitude,
+                    longitude: marker.longitude,
+                  }}
+                />
+              ))}
 
-          <MapView.Marker
-            key={0}
-            coordinate={{
-              latitude: -37.8136,
-              longitude: 144.9631,
-            }}
-          />
-
-          <MapView.Marker
-            key={0}
-            coordinate={{
-              latitude: -33.8688,
-              longitude: 151.2093,
-            }}
-          />
+              {(
+                showDirections &&
+                markers.length === 2
+              ) ? (
+                <MapViewDirections
+                  origin={markers[0]}
+                  destination={markers[1]}
+                  apikey={config.google.apiKey}
+                  strokeColor="blue"
+                  strokeWidth={2}
+                  onReady={this.handleReady}
+                  onError={errorMessage => {
+                    console.warn( 'MapViewDirections error occurred:', errorMessage );
+                  }}
+                />
+                ) : null}
+            </Fragment>
+            ) : null}
         </MapView>
       </Box>
 
