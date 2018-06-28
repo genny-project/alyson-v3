cc5ce0077950627d2bd22658835b0b05067f2df4
zcallan <kaln.delbridge@hotmail.com>
2018-06-28 17:00:28 +1000

    added missing import for user reducer

--- src/views/components/vertx/reducers/index.js
+++ src/views/components/vertx/reducers/index.js
@@ -3,12 +3,14 @@ import baseEntities from './baseEntities.reducer';
 import asks from './asks.reducer';
 import connection from './connection.reducer';
 import aliases from './aliases.reducer';
+import user from './user.reducer';
 
 const reducer = combineReducers({
   baseEntities,
   asks,
   connection,
   aliases,
+  user,
 });
 
 export default reducer;
