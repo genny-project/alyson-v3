94008f8f7c62d3ab1bd8d60294d308288e4240f6
DomHynes <DominicHynes@gmail.com>
2018-07-11 13:27:56 +1000

    fixed dropdown and logout for android, added intl polyfill

--- src/Root.js
+++ src/Root.js
@@ -4,6 +4,7 @@ import App from './views/app';
 import { LayoutProvider } from './views/layout';
 import { VertxProvider, GoogleProvider, ErrorBoundary } from './views/components';
 import { store } from './redux';
+import './polyfills';
 
 console.disableYellowBox = true; // eslint-disable-line no-console
 
