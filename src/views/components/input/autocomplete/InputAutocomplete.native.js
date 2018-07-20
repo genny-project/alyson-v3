d38995059d39d33592514072dca4c6a2b21944ab
DomHynes <DominicHynes@gmail.com>
2018-07-20 14:00:34 +1000

    fixed modal and address input

--- src/views/components/input/autocomplete/InputAutocomplete.native.js
+++ src/views/components/input/autocomplete/InputAutocomplete.native.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Modal, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { func, string, number, oneOfType, array, bool, object } from 'prop-types';
 import Downshift from 'downshift';
-import { Text, Box, Input, Icon, Touchable } from '../../index';
+import { Text, Box, Input, Icon, Touchable, alert, Modal } from '../../index';
 
 class InputAutocomplete extends Component {
   static defaultProps = {
