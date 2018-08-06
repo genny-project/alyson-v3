bcd1b2b30964ff3a45a2acc1c81ab6ddb51793a5
Conor Pirie <conor.pirie@gmail.com>
2018-08-06 17:18:10 +1000

    fixed reducer to properly handle links, updated segemented control component

--- src/views/components/input/segmented-control/SegmentedControl.js
+++ src/views/components/input/segmented-control/SegmentedControl.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import { array, func } from 'prop-types';
+import { array, func, string } from 'prop-types';
 import { isArray } from '../../../../utils';
 import { Box, Text, Touchable } from '../../index';
 
 class Checkbox extends Component {
   static defaultProps = {
-    value: [],
+    backgroundColor: 'white',
+    textColor: '#202532',
+    selectedBackgroundColor: '#202532',
+    selectedTextColor: 'white',
   }
 
   static propTypes = {
     items: array,
     value: array,
     onChangeValue: func,
+    backgroundColor: string,
+    textColor: string,
+    selectedBackgroundColor: string,
+    selectedTextColor: string,
   }
 
   static getDerivedStateFromProps( nextProps, nextState ) {
@@ -19,7 +26,24 @@ class Checkbox extends Component {
       nextProps.value != null &&
       nextProps.value !== nextState.selected
     ) {
+      nextProps.onChangeValue( nextProps.value );
+
       return { selected: nextProps.value };
+    }
+
+    if (
+      nextProps.value == null &&
+      ( nextState.selected === null ||
+        nextState.selected === undefined ) &&
+      nextProps.items != null
+    ) {
+      const item = nextProps.items && nextProps.items[0];
+
+      if ( item && item.value ) {
+        nextProps.onChangeValue( item.value );
+
+        return { selected: item.value };
+      }
     }
 
     return null;
@@ -31,12 +55,12 @@ class Checkbox extends Component {
 
   handlePress = value => () => {
     this.setState( state => {
-      if ( state.selected.includes( value )) {
+      if ( state.selected === value ) {
         return;
       }
 
       return { 
-        selected: [value],
+        selected: value,
       };
     }, () => {
       if ( this.props.onChangeValue ) {
@@ -46,9 +70,15 @@ class Checkbox extends Component {
   }
 
   render() {
-    const { items } = this.props;
+    const {
+      items,
+      backgroundColor,
+      textColor,
+      selectedBackgroundColor,
+      selectedTextColor,
+    } = this.props;
     const { selected } = this.state;
-  
+    
     return (
       <Box
         flexDirection="row"
@@ -59,32 +89,32 @@ class Checkbox extends Component {
             items.map(( item, index ) => {
               const borderCorners = {
                 borderTopLeftRadius: index === 0 ? 10 : 0,
-                borderTopRightRadius: index === items.length ? 10 : 0,
-                borderBottomRightRadius: index === items.length ? 10 : 0,
+                borderTopRightRadius: index === items.length - 1 ? 10 : 0,
+                borderBottomRightRadius: index === items.length - 1 ? 10 : 0,
                 borderBottomLeftRadius: index === 0 ? 10 : 0,
               };
 
               return (
                 <Touchable
                   key={item.value}
-                  onPress={this.handlePress( item.value )}
+                  onPress={selected !== item.value ? this.handlePress( item.value ) : null}
                 >
                   <Box
                     alignItems="center"
                     justifyContent="center"
                     flex={1}
-                    padding={20}
-                    backgroundColor={selected.includes( item.value )
-                      ? '#202532'
-                      : 'white'
+                    padding={15}
+                    backgroundColor={selected === item.value
+                      ? selectedBackgroundColor
+                      : backgroundColor
                     }
                     {...borderCorners}
                   >
                     <Text
-                      size="sm"
-                      color={selected.includes( item.value )
-                        ? 'white'
-                        : '#202532'
+                      size="xs"
+                      color={selected === item.value
+                        ? selectedTextColor
+                        : textColor
                       }
                     >
                       {item.label}
