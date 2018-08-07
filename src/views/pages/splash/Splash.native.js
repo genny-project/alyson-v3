ab4a8f8a859b1a26e33991854e84804cbdd6b9fc
zcallan <kaln.delbridge@hotmail.com>
2018-08-08 09:51:39 +1000

    refactored Splash on web and native and removed redundant code

--- src/views/pages/splash/Splash.native.js
+++ src/views/pages/splash/Splash.native.js
@@ -1,54 +1,34 @@
 import React, { Component } from 'react';
-import { object } from 'prop-types';
-import { Platform, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { LayoutLoader, KeycloakConsumer, Redirect, LayoutFetcher } from '../../components';
 
 class Splash extends Component {
-  static propTypes = {
-    location: object,
-    keycloak: object,
-  }
-
   render() {
-    const { location, keycloak } = this.props;
-
-    const redirectURL = (
-      Platform.OS === 'web' &&
-      location.search.startsWith( '?redirectURL=/' )
-    )
-      ? location.search.split( '?redirectURL=/' )[1]
-      : 'app';
-
     return (
-      <SafeAreaView style={{ flex: 1 }}>
-        {keycloak.isAuthenticated ? (
-          <Redirect
-            to={redirectURL}
-            removeRedirectURL
-            useMainNavigator
-            appTo="home"
-          />
-        ) : (
-          <LayoutFetcher currentUrl="splash">
-            {layout => (
-              <LayoutLoader
-                layout={layout}
+      <KeycloakConsumer>
+        {({ isAuthenticated }) => (
+          <SafeAreaView style={{ flex: 1 }}>
+            {isAuthenticated ? (
+              <Redirect
+                to="app"
+                removeRedirectURL
+                useMainNavigator
+                appTo="home"
               />
+            ) : (
+              <LayoutFetcher currentUrl="splash">
+                {layout => (
+                  <LayoutLoader
+                    layout={layout}
+                  />
+                )}
+              </LayoutFetcher>
             )}
-          </LayoutFetcher>
+          </SafeAreaView>
         )}
-      </SafeAreaView>
+      </KeycloakConsumer>
     );
   }
 }
 
-export default props => (
-  <KeycloakConsumer>
-    {keycloak => (
-      <Splash
-        {...props}
-        keycloak={keycloak}
-      />
-    )}
-  </KeycloakConsumer>
-);
+export default Splash;
