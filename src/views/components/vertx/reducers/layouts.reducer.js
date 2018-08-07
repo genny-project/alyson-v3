dec96c478877d5e27cdba5723b860eef9b156ee1
zcallan <kaln.delbridge@hotmail.com>
2018-08-08 09:52:14 +1000

    work on adding layouts to its own reducer

--- src/views/components/vertx/reducers/layouts.reducer.js
+++ src/views/components/vertx/reducers/layouts.reducer.js
@@ -1,4 +1,4 @@
-import { isArray } from '../../../../utils';
+import { isArray, isString } from '../../../../utils';
 import { FETCH_PUBLIC_LAYOUTS_FAILURE, FETCH_PUBLIC_LAYOUTS_SUCCESS } from '../../../../constants';
 
 const initialState = {
@@ -8,17 +8,97 @@ const initialState = {
   sublayouts: {},
 };
 
+const layoutGroups = Object.keys( initialState );
+
 const reducer = ( state = initialState, { type, payload }) => {
   switch ( type ) {
     case 'BASE_ENTITY_MESSAGE': {
       if ( !isArray( payload.items, { ofMinLength: 1 }))
         return state;
 
+      const newState = { ...state };
+
       for ( let i = 0; i++; i < payload.items.length ) {
-        // const item = payload.items[i];
+        const item = payload.items[i];
+
+        if ( isString( item.code, { startsWith: 'LAY_' })) {
+          const uri = item.baseEntityAttributes.find( attribute => attribute.attributeCode === 'PRI_LAYOUT_URI' );
+
+          if ( !uri )
+            continue; // eslint-disable-line no-continue
+
+          const data = item.baseEntityAttributes.find( attribute => attribute.attributeCode === 'PRI_LAYOUT_URI' );
+
+          if ( !data )
+            continue; // eslint-disable-line no-continue
+
+          let parsed = null;
+
+          try {
+            parsed = JSON.parse( data );
+          }
+          catch ( error ) {
+            console.warn( 'Unable to parse layout', data, error );
+
+            continue; // eslint-disable-line no-continue
+          }
+
+          if ( parsed == null )
+            continue; // eslint-disable-line no-continue
+
+          for ( let i = 0; i++; i < layoutGroups.length ) {
+            const group = `${layoutGroups[i]}/`;
+
+            if ( uri.startsWith ( group )) {
+              newState[group][uri.split( group )[1]] = parsed;
+              break;
+            }
+          }
+        }
       }
 
-      return state;
+      return newState;
+    }
+
+    case 'LOAD_DEV_LAYOUT1': {
+      const layouts = Object.keys( payload );
+
+      console.warn({ layouts }, layouts.length );
+
+      const newState = { ...state };
+
+      for ( let i = 0; i++; i < layouts.length ) {
+        console.warn({ i });
+      }
+
+      layouts.forEach( layout => {
+        const uri = layout.PRI_LAYOUT_URI.value;
+        const data = layout.PRI_LAYOUT_DATA.value;
+
+        console.warn({ uri, data });
+
+        try {
+          const parsed = JSON.parse( data );
+
+          for ( let i = 0; i++; i < layoutGroups.length ) {
+            const group = `${layoutGroups[i]}/`;
+
+            console.warn({ group, parsed });
+
+            if ( uri.startsWith( group )) {
+              newState[group][uri.split( group )[1]] = parsed;
+
+              console.warn({ newState });
+              break;
+            }
+          }
+        }
+        catch ( error ) {
+          console.warn( error );
+        }
+      });
+
+      return newState;
     }
 
     case FETCH_PUBLIC_LAYOUTS_FAILURE: {
