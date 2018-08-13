185c8c52eb3396e90ed8e6617145653f636d0bd7
zcallan <kaln.delbridge@hotmail.com>
2018-08-13 14:31:23 +1000

    removed deprecated LoginForm components

--- src/views/components/login/form/index.js
+++ src/views/components/login/form/index.js
@@ -1,4 +1,1 @@
 export { default } from './LoginForm';
-export { default as LoginFormInput } from './input';
-export { default as LoginFormSubmit } from './submit';
-export { default as LoginFormError } from './error';
