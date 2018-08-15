df239ad8efb00fa343350e7ef4cabc1fd1038efe
Adam Crow <acrow@Adams-Compooter.local>
2018-08-15 11:53:52 +1000

    Updated docker build

--- push.sh
+++ push.sh
@@ -1,1 +1,13 @@
-docker push genny-project/alyson-v3
+#!/bin/bash
+
+if [ -z "${1}" ]; then
+   version="latest"
+else
+   version="${1}"
+fi
+
+
+docker push gennyproject/alyson-v3:${version}
+docker tag  gennyproject/alyson-v3:${version} gennyproject/alyson-v3:latest
+docker push gennyproject/alyson-v3:latest
+
