2c3e709039afd914ebc80c5c05cb7cfbb7466d9e
Nischal Gautam <nischal1024@gmail.com>
2018-08-23 14:17:02 +1000

    added display HTML component

---
+++ src/views/components/display-html/DisplayHTML.native.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Html from 'react-native-render-html';
+import { string } from 'prop-types';
+import { Box } from '../../components';
+
+class DisplayHTML extends Component {
+  static propTypes = { 
+    content: string,
+  };
+
+  render() { 
+    const { content } = this.props;
+
+    return (
+      <Box>
+        <Html
+          html={content}
+        />
+      </Box>
+    );
+  }
+}
+
+export default DisplayHTML;
---
+++ src/views/components/display-html/DisplayHTML.web.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { string } from 'prop-types';
+import { Box } from '../../components';
+
+class DisplayHTML extends Component {
+  static propTypes = { 
+    content: string,
+  };
+
+  render() { 
+    const { content } = this.props;
+
+    return (
+      <Box>
+        <div
+          dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+            __html: content,
+          }}
+        />
+      </Box>
+    );
+  }
+}
+
+export default DisplayHTML;
