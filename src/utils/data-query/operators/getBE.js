64b87b3fa9332d625c33818c99401a1f880c9486
zcallan <kaln.delbridge@hotmail.com>
2018-09-12 17:19:41 +1000

    undefined fields are now removed from data in getBE data query

--- src/utils/data-query/operators/getBE.js
+++ src/utils/data-query/operators/getBE.js
@@ -1,22 +1,34 @@
 import copy from 'fast-copy';
 import { injectContext } from './helpers';
+import { isArray, isObject } from '../../../utils';
 
 export default ( data, options, allData ) => {
-  if ( !data ) {
+  if ( !data )
     return data;
-  }
 
-  return data.length ? data.map( item => lookupBE( item, options, allData )) :
-    lookupBE( data, options, allData );
+  return !isArray( data )
+    ? lookupBE( data, options, allData )
+    : data.reduce(( result, item ) => {
+      const be = lookupBE( item, options, allData );
+
+      if ( be === -1 )
+        return result;
+
+      result.push( be );
+
+      return result;
+    }, [] );
 };
 
 const lookupBE = ( data, options, allData ) => {
-  if ( typeof data !== 'object' ) {
+  if ( !isObject( data ))
     return;
-  }
 
   /* Create the path to the base entity */
   const be = copy( allData.baseEntities.data[injectContext( options.id, data )] );
+
+  if ( !be )
+    return -1;
 
   return options.as ? { ...data, [options.as]: be } : be;
 };
