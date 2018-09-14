a90e060b45ccce8d7338badd1d7be635d8253217
Nischal Gautam <nischal1024@gmail.com>
2018-09-14 11:36:01 +1000

    map changes

--- src/views/components/map/Map.web.js
+++ src/views/components/map/Map.web.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { GoogleProvider } from '../google';
 
 class Map extends Component {
   constructor( props ) {
@@ -9,19 +8,11 @@ class Map extends Component {
 
   render() {
     return (
-      <div> 
-        <GoogleProvider
-          scriptTagId="google-map-web"
-        >
-          <div
-            style={{ height: 200, width: 200 }}
-            className="google-map-web"
-            id="google-map-web"
-            ref={this.myRef}
-          />
-        </GoogleProvider>
+      <div>
+        <h1>
+        Maps component
+        </h1>
       </div>
-      
     );
   }
 }
