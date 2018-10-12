635ae7d4c861046aa8d09ffb2d242ba7b95cd78f
zcallan <kaln.delbridge@hotmail.com>
2018-10-12 13:53:16 +1100

    improved type checking in $or and $nor

--- src/utils/data-query/operators/findOperators/$or.js
+++ src/utils/data-query/operators/findOperators/$or.js
@@ -1,3 +1,8 @@
+import { isArray } from '../../../../utils';
+
 export default ( input, value, doesValueMatch ) => {
+  if ( !isArray( value ))
+    return false;
+
   return !!value.find( operators => doesValueMatch( operators ));
 };
