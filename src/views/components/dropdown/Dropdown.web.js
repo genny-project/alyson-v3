5e4bf3d14f7c871ead1b9a724c8f336f2801cff9
zcallan <kaln.delbridge@hotmail.com>
2018-10-19 16:00:50 +1100

    updated Dropdown web, refactor Link

--- src/views/components/dropdown/Dropdown.web.js
+++ src/views/components/dropdown/Dropdown.web.js
@@ -1,77 +1,20 @@
-import React, { Component, Fragment } from 'react';
-import { TouchableOpacity, TouchableWithoutFeedback, Animated } from 'react-native';
-import { array, string, bool, number, node } from 'prop-types';
-import { Box, Text, Icon } from '../../components';
-import DropdownItem from './item';
+import React, { Component, isValidElement } from 'react';
+import { Menu, MenuButton, MenuList, MenuLink } from '@reach/menu-button';
+import { array, bool, object, any } from 'prop-types';
+import { isArray, isString } from '../../../utils';
+import { Recursive } from '../../components';
+import './Dropdown.css';
 
 class Dropdown extends Component {
-  static defaultProps = {
-    padding: 20,
-    backgroundColor: '#FFF',
-    textColor: '#000',
-  }
-
   static propTypes = {
     items: array.isRequired,
-    text: node.isRequired,
+    text: any,
     facingRight: bool,
-    padding: number,
-    paddingX: number,
-    paddingY: number,
-    backgroundColor: string,
-    textColor: string,
     disabled: bool,
-  }
-
-  state = {
-    isOpen: false,
-    fadeAnim: new Animated.Value( 0 ),
-    dropdownScaleY: new Animated.Value( 0 ),
-  }
-
-  handleOpen = () => {
-    if ( this.props.disabled )
-      return;
-
-    this.setState({ isOpen: true });
-
-    Animated.parallel( [
-      Animated.timing(
-        this.state.fadeAnim,
-        {
-          toValue: 1,
-          duration: 100,
-        }
-      ),
-      Animated.timing(
-        this.state.dropdownScaleY,
-        {
-          toValue: 1,
-          duration: 120,
-        }
-      ),
-    ] ).start();
-  }
-
-  handleClose = () => {
-    Animated.parallel( [
-      Animated.timing(
-        this.state.fadeAnim,
-        {
-          toValue: 0,
-          duration: 250,
-        }
-      ),
-      Animated.timing(
-        this.state.dropdownScaleY,
-        {
-          toValue: 0,
-          duration: 300,
-        }
-      ),
-    ] ).start(() => {
-      this.setState({ isOpen: false });
-    });
+    menuButtonStyle: object,
+    menuListStyle: object,
+    menuLinkStyle: object,
+    children: any,
   }
 
   render() {
@@ -79,122 +22,58 @@ class Dropdown extends Component {
       items,
       text,
       facingRight,
-      padding,
-      paddingX,
-      paddingY,
-      textColor,
-      backgroundColor,
+      menuButtonStyle,
+      menuListStyle,
+      menuLinkStyle,
+      disabled,
+      children,
     } = this.props;
 
-    const { isOpen, fadeAnim, dropdownScaleY } = this.state;
-
     return (
-      <Fragment>
-        {isOpen && (
-          <Animated.View
+      <Menu>
+        <MenuButton
+          disabled={disabled || !isArray( items, { ofMinLength: 1 })}
+          style={menuButtonStyle}
+        >
+          {isValidElement( children ) ? children
+          : isString( text ) ? text
+          : isArray( children )
+            ? children.map(( child, i ) => (
+              isValidElement( child )
+                ? child
+                : <Recursive key={i} {...child} /> // eslint-disable-line
+            ))
+            : <Recursive {...children} />
+          }
+        </MenuButton>
+
+        {isArray( items, { ofMinLength: 1 }) && (
+          <MenuList
             style={{
-              opacity: fadeAnim,
+              position: 'absolute',
+              top: '100%',
+              ...facingRight
+                ? { right: 0 }
+                : { left: 0 },
+              ...menuListStyle,
             }}
           >
-            <TouchableWithoutFeedback
-              onPress={this.handleClose}
-            >
-              <Box
-                position="fixed"
-                top={0}
-                left={0}
-                height="100%"
-                width="100%"
-                zIndex={74}
-                backgroundColor="#000"
-                opacity={0.1}
-              />
-            </TouchableWithoutFeedback>
-          </Animated.View>
-        )}
-
-        <Box
-          position="relative"
-          zIndex={755}
-        >
-          <TouchableOpacity
-            onPress={this.handleOpen}
-          >
-            <Box
-              justifyContent="space-between"
-              alignItems="center"
-              padding={padding}
-              paddingX={paddingX}
-              paddingY={paddingY}
-            >
-              {typeof text === 'string' ? (
-                <Text
-                  color={textColor}
-                >
-                  {text}
-                </Text>
-              ) : text}
-
-              <Box width={5} />
-
-              <Icon
-                name="expand-more"
-                color={textColor}
-              />
-            </Box>
-          </TouchableOpacity>
-
-          {isOpen && (
-            <Animated.View
-              style={{
-                transform: [
-                  { scaleY: dropdownScaleY },
-                  { scaleX: dropdownScaleY },
-                ],
-                opacity: fadeAnim,
-                position: 'absolute',
-                height: '100%',
-                width: '100%',
-              }}
-            >
-              <Box
-                position="absolute"
-                top="100%"
-                backgroundColor={backgroundColor}
-                flexDirection="column"
-                minWidth={170}
-                {...facingRight
-                  ? { right: 0 }
-                  : { left: 0 }
-              }
+            {items.map( item => (
+              <MenuLink
+                key={item.text}
+                to={(
+                  item.href === 'home' ? '/'
+                  : item.href.startsWith( '/' ) ? item.href
+                  : `/${item.href}`
+                )}
+                style={menuLinkStyle}
               >
-                {(
-                  items &&
-                  items instanceof Array &&
-                  items.length > 0
-                ) ? (
-                    items.map( item => (
-                      <DropdownItem
-                        text={item.text}
-                        icon={item.icon}
-                        key={item.text}
-                        href={item.href}
-                        onPress={this.handleClose}
-                        textColor={textColor}
-                      />
-                    ))
-                  ) : (
-                    <DropdownItem
-                      text="No items to show."
-                      onPress={this.handleClose}
-                      textColor={textColor}
-                    />
-                  )}
-              </Box>
-            </Animated.View>
-          )}
-        </Box>
-      </Fragment>
+                {item.text}
+              </MenuLink>
+            ))}
+          </MenuList>
+        )}
+      </Menu>
     );
   }
 }
