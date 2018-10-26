3c3354eddf910fdca4b33f780641325722dc5889
Nischal Gautam <nischal1024@gmail.com>
2018-10-26 16:42:10 +1100

    part of merge modifications

--- ios/alysonv3/AppDelegate.m
+++ ios/alysonv3/AppDelegate.m
@@ -20,6 +20,8 @@
 {
   NSURL *jsCodeLocation;
 
+  [AppCenterReactNative register];  // Initialize AppCenter 
+
   [AppCenterReactNativeCrashes registerWithAutomaticProcessing];  // Initialize AppCenter crashes
 
   [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];  // Initialize AppCenter analytics
