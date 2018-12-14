f7c498b78f2f3d1c2bfa1c0d255e0db18b0ad1c8
Nischal Gautam <nischal1024@gmail.com>
2018-12-14 11:48:36 +1100

    convert to functional comp

--- src/views/components/pdf-viewer/PdfViewer.web.js
+++ src/views/components/pdf-viewer/PdfViewer.web.js
@@ -1,43 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { oneOfType, string, number, object } from 'prop-types';
 
-class Pdf extends Component {
-  static defaultProps = {
-    style: {},
-    height: '600px',
-    width: '600px',
-  };
+const  Pdf  = ({ style = {}, height =  '600px', width = '600px', file = null }) => {
+  const componentStyle = style;
 
-  static propTypes = {
-    width: oneOfType( [number, string] ),
-    height: oneOfType( [number, string] ), 
-    style: object,
-    file: string,
-  };
-
-  render() {
-    const { width, height, style, file } = this.props;
-    const componentStyle = style;
-
-    return (
-      <div
-        style={
+  return (
+    <div
+      style={
             { height: height, width: width, ...componentStyle  }
           }
-      >
-        { file !== null && file !== undefined && file !== 'undefined' ? (
-          <iframe
-            title="pdf-document"
-            src={file}
-            width="100%"
-            height="100%"
-          />
-        ) : 'Pdf Document Loading Please Wait'
+    >
+      { file !== null && file !== undefined && file !== 'undefined' ? (
+        <iframe
+          title="pdf-document"
+          src={file}
+          width="100%"
+          height="100%"
+        />
+      ) : 'Pdf Document Loading Please Wait'
         }
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Pdf.propTypes = { 
+  width: oneOfType( [number, string] ),
+  height: oneOfType( [number, string] ), 
+  style: object,
+  file: string,
+};
 
 export default Pdf;
 
