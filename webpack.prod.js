8a188cf039728c07abc13d21b8dee0651cead46e
rpgayatri <rpgayatri@gmail.com>
2019-01-15 10:55:49 +1100

    enabling env file to be read in docker mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,40 @@
 // const BundleAnalyzerPlugin = require( 'webpack-bundle-analyzer' ).BundleAnalyzerPlugin;
+require( 'dotenv/config' );
+const webpack = require( 'webpack' );
 const merge = require( 'webpack-merge' );
 const common = require( './webpack.common.js' );
+const Dotenv = require( 'dotenv-webpack' );
 
-module.exports = merge( common, {
-  plugins: [
-    // new BundleAnalyzerPlugin(),
-  ],
-});
+/**
+ * Prefix each key in an object with `process.env`,
+ * and set the value of each key to be stringified.
+ *
+ * @param {object} envVars
+ */
+function formatEnvironmentVariables( envVars ) {
+  const keys = Object.keys( envVars );
+
+  return keys.reduce(( result, key ) => {
+    result[`process.env.${key}`] = JSON.stringify( envVars[key] );
+
+    return result;
+  }, {});
+}
+
+module.exports = env => {
+  const plugins = [
+    new Dotenv({ path: './.env' }),
+  ];
+
+  if ( env ) {
+    const envVars = formatEnvironmentVariables( env );
+
+    plugins.push(
+      new webpack.DefinePlugin( envVars )
+    );
+  }
+
+  return merge( common, {
+    plugins,
+  });
+};
