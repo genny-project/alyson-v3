22fab508bcedb53e15acd3e4427df1c4d691e8de
conorpirie <conor.pirie@gmail.com>
2019-01-14 11:32:45 +1100

    update getBE

--- src/utils/data-query/operators/getBE.js
+++ src/utils/data-query/operators/getBE.js
@@ -3,13 +3,19 @@ import { injectContext } from './helpers';
 import { isArray, isObject, isString } from '../../../utils';
 
 export default ( data, options, allData ) => {
+  // if ( isArray( data )) console.log( '-', data, data.length );
+
   if ( !data )
     return data;
 
   return !isArray( data )
     ? lookupBE( data, options, allData )
     : data.reduce(( result, item ) => {
+      // console.log( '- -', item );
+
       const be = lookupBE( item, options, allData );
+
+      // console.log( '- -', be );
 
       if (
         options.filterOutEmpty &&
@@ -25,17 +31,25 @@ export default ( data, options, allData ) => {
 };
 
 const lookupBE = ( data, options, allData ) => {
+  // console.log( '- - -', 'data', data );
   const lookupKey = (
     isString( data ) ? data
     : isObject( data ) ? injectContext( options.id, data )
     : null
   );
 
+  // console.log( '- - -', 'lookupKey', lookupKey );
+
   if ( !lookupKey )
     return;
 
   /* Create the path to the base entity */
   const be = copy( allData.baseEntities.data[lookupKey] );
+
+  // console.log( '- - -', 'be', be );
+
+  if ( be === undefined )
+    return;
 
   if (
     options.filterOutEmpty && (
@@ -46,6 +60,8 @@ const lookupBE = ( data, options, allData ) => {
   ) {
     return undefined;
   }
+
+  // console.log( '- - -', 'is  array?', isArray( be ), typeof be );
 
   return options.as ? { ...data, [options.as]: be } : be;
 };
