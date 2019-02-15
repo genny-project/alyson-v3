afe5058ae9fbac4ebaac71c6bba02513075e2329
Nischal Gautam <nischal1024@gmail.com>
2019-02-15 11:41:06 +1100

    show codes set from local storage and persist

--- src/views/components/tooltip/testid-tooltip/TestIdTooltip.js
+++ src/views/components/tooltip/testid-tooltip/TestIdTooltip.js
@@ -2,32 +2,51 @@ import React, { Component } from 'react';
 import { Dimensions } from 'react-native-web';
 import { string, node } from 'prop-types';
 import Tooltip from '../Tooltip';
+import Storage from '../../../../utils/storage';
 
 class TestIdTooltip extends Component {
   static defaultProps = {
     id: 'test-id',
-  }
+  };
 
   static propTypes = {
     id: string,
     children: node,
+  };
+
+  // persist the showcodes on localstorgae
+  async persistCodes() {
+    const { children } = this.props;
+    let isDebugMode;
+
+    if ( typeof window === 'object' ) {
+      isDebugMode = window.originalQueryParams.showcodes;
+    }
+
+    const code = await Storage.get( 'showcodes' );
+
+    if ( code === true || code === 'true' ) {
+      isDebugMode = true;
+    } else {
+      isDebugMode = false;
+    }
+
+    if ( !isDebugMode ) {
+      return children;
+    }
   }
 
   handleMouseOver = () => {
     // eslint-disable-next-line no-console
     console.warn( 'TestId:', this.props.id );
-  }
+  };
 
   render() {
     const { id, children } = this.props;
 
     const dimensions = Dimensions.get( 'window' );
 
-    const isDebugMode = window.originalQueryParams.showcodes;
-
-    if ( !isDebugMode ) {
-      return children;
-    }
+    this.persistCodes();
 
     return (
       <Tooltip
